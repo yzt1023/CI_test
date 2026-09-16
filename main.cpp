f59d4efefc250ee3cd80f07ff672e53a1a10a7c0
SkyeYou <yzt1023@126.com>
2026-09-16 14:29:10 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+int test() {
+    int *p = nullptr;
+    *p = 10;
+	return 0;
+}
+
 int main() {
     std::cout << "hello world" << std::endl;
-    std::cout << "test PR" << std::endl;
+  std::cout << "test code coverity" << std::endl;
+	test();
 }
