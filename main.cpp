99214e8410383003f8e7675457440b0c0a157459
wind <Skye.You@coherent.com>
2026-09-16 09:24:09 +0800

    test static analyzer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+int test() {
+    int *p = nullptr;
+    *p = 10;
+}
+
 int main() {
     std::cout << "hello world" << std::endl;
-    std::cout << "test PR" << std::endl;
+  std::cout << "test code coverity" << std::endl;
+	test();
 }
